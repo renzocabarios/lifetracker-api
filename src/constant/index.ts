247aa3db373acebfd343dbbb78f1c088e3e72756
Renzo Cabarios <renzo.cabarios@gmail.com>
2022-10-14 12:34:39 +0800

    Remove message constant

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -1,4 +1,3 @@
 import STATUS from './status';
-import MESSAGE from './message';
 
-export { STATUS, MESSAGE };
+export { STATUS };
--- src/constant/message.ts
+++
@@ -1,8 +0,0 @@
-export default {
-  SUCCESS_FETCH: 'Successfully fetched',
-  SUCCESS_CREATE: 'Successfully created',
-  SUCCESS_UPDATE: 'Successfully updated',
-  SUCCESS_DELETE: 'Successfully deleted',
-};
-
-// TODO: TURN MESSAGES INTO A UTILITY
