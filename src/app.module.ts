a7599451c6e281140c562aaf934987466a61d685
Renzo Cabarios <renzo.cabarios@gmail.com>
2022-10-14 12:34:06 +0800

    Add user resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,14 +4,18 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import environment from './config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ResponseService } from './services';
+import { UserModule } from './v1';
+
 @Module({
   imports: [
     MongooseModule.forRoot(environment.MONGOD_DB),
     ConfigModule.forRoot({
       load: [() => ({ environment })],
     }),
+    UserModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ResponseService],
 })
 export class AppModule {}
